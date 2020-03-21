a7885f4c22d4ae3efcdd05c8433bc01007ed84ed
franicrack <franicrackpedraja@gmail.com>
2020-03-21 18:33:48 +0100

    Falta la operasion super

--- src/main/java/es/sd/CuadroVendido.java
+++ src/main/java/es/sd/CuadroVendido.java
@@ -6,5 +6,19 @@ public class CuadroVendido extends Cuadro{
     private String comprador;
     private Date fechaDeCompra;
 
-    
+    public String getComprador() {
+        return comprador;
+    }
+
+    public void setComprador(String comprador) {
+        this.comprador = comprador;
+    }
+
+    public Date getFechaDeCompra() {
+        return fechaDeCompra;
+    }
+
+    public void setFechaDeCompra(Date fechaDeCompra) {
+        this.fechaDeCompra = fechaDeCompra;
+    }
 }
