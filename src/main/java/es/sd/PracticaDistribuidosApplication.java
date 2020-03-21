7fcfecf530516a18e070f8133761717b90772b40
chaarlie37 <carlos37sm@gmail.com>
2020-03-21 16:48:50 +0100

    Initial commit

---
+++ src/main/java/es/sd/PracticaDistribuidosApplication.java
@@ -0,0 +1,13 @@
+package es.sd;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PracticaDistribuidosApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PracticaDistribuidosApplication.class, args);
+	}
+
+}
---
+++ src/test/java/es/sd/PracticaDistribuidosApplicationTests.java
@@ -0,0 +1,13 @@
+package es.sd;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PracticaDistribuidosApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
