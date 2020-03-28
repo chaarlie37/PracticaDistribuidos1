d210cd1363c098fd3a98abcd3833de09d59a96a3
chaarlie37 <carlos37sm@gmail.com>
2020-03-28 18:10:03 +0100

    Detalles + Modificacion clientes + no permitir NIF duplicados

--- src/main/java/sd/practica1/controllers/ClientesController.java
+++ src/main/java/sd/practica1/controllers/ClientesController.java
@@ -4,10 +4,12 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import sd.practica1.model.Cliente;
 import sd.practica1.repositories.ClienteRepository;
 
 import javax.annotation.PostConstruct;
+import java.util.Optional;
 
 @Controller
 public class ClientesController {
@@ -35,7 +37,36 @@ public class ClientesController {
 
     @RequestMapping("/guardarcliente")
     public String guardarCliente(Cliente cliente,Model model){
-        clienteRepository.save(cliente);
+        if(clienteRepository.findByNIF(cliente.getNIF()) == null){
+            clienteRepository.save(cliente);
+            return "exito";
+        }
+        else{
+            String mensaje = "Error: El cliente con NIF " + cliente.getNIF() + " ya existe.";
+            model.addAttribute("mensaje", mensaje);
+            return "error_msg";
+        }
+
+    }
+
+    @RequestMapping("/mostrarcliente")
+    public String mostrarCliente(@RequestParam String nif, Model model){
+        Cliente cliente = clienteRepository.findByNIF(nif);
+        model.addAttribute("cliente", cliente);
+        return "mostrar_cliente";
+    }
+
+    @RequestMapping("/modificarclienteform")
+    public String modificarClienteForm(@RequestParam String nif, Model model){
+        Cliente cliente = clienteRepository.findByNIF(nif);
+        model.addAttribute("cliente", cliente);
+        return "modificarclienteform";
+    }
+
+    @RequestMapping("/modificarcliente")
+    public String modificarCliente(@RequestParam Integer id, Cliente nuevo, Model model){
+        nuevo.setId(id);
+        clienteRepository.save(nuevo);
         return "exito";
     }
 }
--- src/main/java/sd/practica1/repositories/ClienteRepository.java
+++ src/main/java/sd/practica1/repositories/ClienteRepository.java
@@ -3,5 +3,9 @@ package sd.practica1.repositories;
 import org.springframework.data.jpa.repository.JpaRepository;
 import sd.practica1.model.Cliente;
 
+import java.util.Optional;
+
 public interface ClienteRepository extends JpaRepository <Cliente, Integer> {
+    Cliente findByNIF(String nif);
+    Optional<Cliente> findById(Integer id);
 }
