47cc3db4b00d9b17fb720501e066edab5a4f3322
chaarlie37 <carlos37sm@gmail.com>
2020-04-02 22:14:53 +0200

    iconos

--- src/main/java/sd/practica1/controllers/ClientesController.java
+++ src/main/java/sd/practica1/controllers/ClientesController.java
@@ -22,10 +22,10 @@ public class ClientesController {
 
     @PostConstruct
     public void init(){
-        Cliente c1 = new Cliente("Cliente1","aaa","aaa","aaa","aaa","aaa");
-        Cliente c2 = new Cliente("Cliente2","bbb","bbb","bbb","bbb","bbb");
-        Cliente c3 = new Cliente("Cliente3","ccc","ccc","ccc","ccc","ccc");
-        Cliente c4 = new Cliente("Cliente4","ddd","ddd","ddd","ddd","ddd");
+        Cliente c1 = new Cliente("Carlos","Sanchez Muñoz","987654321A","Calle Aaaaaaa, 327","direccion@gmail.com","123456789");
+        Cliente c2 = new Cliente("Francisco","Pedraja Alonso","789456123B","Calle Kkkkkkkk, 27, Aranjuez","tonto@gmail.com","254987321");
+        Cliente c3 = new Cliente("Lidia","Hernandez Calvo","123456789N","Calle Cccccccccc, 72, Valdemoro","rubia@gmail.com","456987123");
+        Cliente c4 = new Cliente("Willyrex","Señor Youtuber","951753654P","Calle lo que Sea, 77, Andorra","manco@machinima.com","777777777");
         clienteRepository.save(c1);
         clienteRepository.save(c2);
         clienteRepository.save(c3);
